3b85d431b0e6fc9eb2e4b495696f21b966a776f9
FabioVazzz <FabioTiana15@outlook.com>
2025-01-14 13:23:33 -0300

    Create bandgenerator.py

---
+++ bandgenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator")
+
+cityName = input("Enter the city you born\n")
+yourName = input("Enter your name\n")
+print(f"Your band name is {cityName} {yourName}")
